3c26c277d59f513f71490c5ac2a2f361c5114f93
zack <booooomzack@gmail.com>
2025-04-16 20:53:25 +0800

    Align map generating to grid.

--- Assets/Scripts/MapController.cs
+++ Assets/Scripts/MapController.cs
@@ -30,8 +30,9 @@ public class MapController : MonoBehaviour
         {
             //预期位置是移动方向+地图块大小
             newChunkPos = new Vector2(player.position.x, player.position.y) + playerMovement.moveDir * MapCheckRadius;
+            newChunkPos = AlignNewChunkPos(newChunkPos, MapCheckRadius * 2);
             Debug.Log("newChunkPos=" + newChunkPos);
-            Collider2D[] existedChunks = Physics2D.OverlapCircleAll(newChunkPos, MapCheckRadius, terrianLayer);
+            Collider2D[] existedChunks = Physics2D.OverlapCircleAll(newChunkPos, MapCheckRadius/2, terrianLayer);
             if (existedChunks.Length == 0)
             {
                 Instantiate(TerrianChunk, newChunkPos, Quaternion.identity).layer = terrianLayer;
@@ -39,12 +40,17 @@ public class MapController : MonoBehaviour
         }
     }
 
+    //对齐生成的地图位置到网格
+    Vector2 AlignNewChunkPos(Vector2 pos, float gridSize)
+    {
+        float x = Mathf.Round(pos.x / gridSize) * gridSize;
+        float y = Mathf.Round(pos.y / gridSize) * gridSize;
+        return new Vector2 (x, y);
+    }
+
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(player.position, newChunkPos);
-
-        Gizmos.color = Color.white;
-        Gizmos.DrawWireSphere(newChunkPos, MapCheckRadius);
+        Gizmos.DrawWireSphere(newChunkPos, MapCheckRadius/2);
     }
 }
